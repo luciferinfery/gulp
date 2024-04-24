2ab780cc7d9afac1982b5f6da70b13a710f84bad
luciferinfery <rudfedor@gmail.com>
2024-04-25 00:00:27 +0300

    Gulp HW_2

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,33 @@
 const gulp = require('gulp');
 
 gulp.task('html', function() {
-    return gulp.src('layout-projec/**/*.html')
-        .pipe(gulp.dest('dist'));
+    return gulp.src('layout-project/src/html/**/*.html')
+        .pipe(gulp.dest('layout-project/dist/html'));
 });
 
 gulp.task('css', function() {
-    return gulp.src('layout-projec/**/*.css')
-        .pipe(gulp.dest('dist'));
+    return gulp.src('layout-project/src/css/**/*.css')
+        .pipe(gulp.dest('layout-project/dist/css'));
 });
 
 gulp.task('js', function() {
-    return gulp.src('layout-projec/**/*.js')
-        .pipe(gulp.dest('dist'));
+    return gulp.src('layout-project/src/js/**/*.js')
+        .pipe(gulp.dest('layout-project/dist/js'));
 });
 
-gulp.task('default', gulp.parallel('html', 'css', 'js'));
+gulp.task('img', function() {
+    return gulp.src('layout-project/src/img/**/*')
+        .pipe(gulp.dest('layout-project/dist/img'));
+});
+
+gulp.task('fonts', function() {
+    return gulp.src('add/fonts/**/*')
+        .pipe(gulp.dest('layout-project/dist/fonts'));
+});
+
+gulp.task('data', function() {
+    return gulp.src('add/data/**/*')
+        .pipe(gulp.dest('layout-project/dist/data'));
+});
+
+gulp.task('default', gulp.parallel('html', 'css', 'js', 'img', 'fonts', 'data'));
