14374bac1d59949335428164b3f619d5f09a874e
luciferinfery <rudfedor@gmail.com>
2024-04-25 13:52:41 +0300

    Gulp HW_4

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,33 @@
-const gulp = require('gulp');
+const { src, dest, watch, series } = require('gulp');
+const tinify = require('gulp-tinify');
+const uglify = require('gulp-uglify');
+const sass = require('gulp-sass')(require('sass'));
 
-gulp.task('html', function() {
-    return gulp.src('layout-projec/**/*.html')
-        .pipe(gulp.dest('dist'));
-});
+function compileSass() {
+    console.log('Compiling Sass files...');
+    return src('layout-projec/src/sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(dest('dist/css'));
+}
 
-gulp.task('css', function() {
-    return gulp.src('layout-projec/**/*.css')
-        .pipe(gulp.dest('dist'));
-});
+function compressImages() {
+    console.log('Compressing images...');
+    return src('layout-projec/src/img/**/*')
+        .pipe(tinify('p0m2l0vQvpGqZhjcJnSzTbxjM0KfFhW8'))
+        .pipe(dest('dist/img'));
+}
 
-gulp.task('js', function() {
-    return gulp.src('layout-projec/**/*.js')
-        .pipe(gulp.dest('dist'));
-});
+function minifyJS() {
+    console.log('Minifying JS files...');
+    return src('layout-projec/src/js/**/*.js')
+        .pipe(uglify())
+        .pipe(dest('dist/js'));
+}
 
-gulp.task('default', gulp.parallel('html', 'css', 'js'));
+function watchFiles() {
+    watch('layout-projec/src/sass/**/*.scss', compileSass);
+    watch('layout-projec/src/img/**/*', compressImages);
+    watch('layout-projec/src/js/**/*.js', minifyJS);
+}
+
+exports.default = series(compileSass, compressImages, minifyJS, watchFiles);
